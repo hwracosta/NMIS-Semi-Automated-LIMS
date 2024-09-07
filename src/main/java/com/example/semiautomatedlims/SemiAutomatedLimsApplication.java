63faf8c7abe41539184b022a5476ff5c672c0066
Harry Acosta <hracosta@up.edu.ph>
2024-09-07 19:52:53 +0800

    Project Structure

---
+++ src/main/java/com/example/semiautomatedlims/SemiAutomatedLimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.semiautomatedlims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiAutomatedLimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiAutomatedLimsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/semiautomatedlims/SemiAutomatedLimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semiautomatedlims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiAutomatedLimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
