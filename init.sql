5b3a012ddef8931e2206b4d99c34ccabd12176b3
ljdvr <lddevera@up.edu.ph>
2024-09-11 19:40:25 +0800

    Added id column  to staff register and client register tables

--- init.sql
+++ init.sql
@@ -4,10 +4,12 @@ CREATE TABLE CLIENT_register (
     representative_name VARCHAR(255) NOT NULL,
     contact_number VARCHAR(20) NOT NULL,
     company_name VARCHAR(255) NOT NULL
+    client_id SERIAL PRIMARY KEY,
 );
 
 CREATE TABLE STAFF_register (
-    email VARCHAR(255) PRIMARY KEY,
+    staff_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     staff_type VARCHAR(50) NOT NULL
 );
