8218efd83d077218ac0bb8d82d947bbbb1a3a7bf
JuyeonCodez <lglumain@up.edu.ph>
2024-09-11 18:15:07 +0800

    Rename Cliet and Staff FPW controller

--- src/main/java/com/example/semiautomatedlims/Controller/StaffFPWController.java
+++ src/main/java/com/example/semiautomatedlims/Controller/StaffFPWController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class StaffFPW {
+public class StaffFPWController {
     @GetMapping("/STAFF-fpw")
     public String staffForgotPassword() {
         return "STAFF-fpw"; // This will look for a staff-fpw.html file in the templates folder
