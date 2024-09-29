76fe647f4f997880a7646dfd769ceaa0fd189810
ljdvr <lddevera@up.edu.ph>
2024-09-29 21:23:43 +0800

    Working RELEASE-review page

--- src/main/java/com/example/semiautomatedlims/Controller/ReleaseReviewController.java
+++ src/main/java/com/example/semiautomatedlims/Controller/ReleaseReviewController.java
@@ -1,12 +1,28 @@
 package com.example.semiautomatedlims.Controller;
 
+import com.example.semiautomatedlims.Entity.ClientReqForm;
+import com.example.semiautomatedlims.Service.ClientReqFormService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ReleaseReviewController {
+
+    @Autowired
+    private ClientReqFormService clientReqFormService;
+
     @GetMapping("/RELEASE-review")
-    public String releaseReview() {
+    public String releaseReview(Model model) {
+        
+        // Fetch all client requests
+        List<ClientReqForm> requests = clientReqFormService.getAllClientRequests();
+        model.addAttribute("requests", requests); 
+
         return "RELEASE-review";
     }
 }
+
