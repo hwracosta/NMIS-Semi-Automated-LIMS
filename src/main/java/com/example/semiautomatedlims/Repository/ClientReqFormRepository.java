aa26e0c177409d28305ab3ca81a669adfc79f9d3
ljdvr <lddevera@up.edu.ph>
2024-10-05 23:19:46 +0800

    For Testing requests are reflected

--- src/main/java/com/example/semiautomatedlims/Repository/ClientReqFormRepository.java
+++ src/main/java/com/example/semiautomatedlims/Repository/ClientReqFormRepository.java
@@ -1,16 +1,18 @@
 package com.example.semiautomatedlims.Repository;
 
-import com.example.semiautomatedlims.Entity.ClientReqForm;
-import com.example.semiautomatedlims.Entity.Client;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import com.example.semiautomatedlims.Entity.Client;
+import com.example.semiautomatedlims.Entity.ClientReqForm;
 
 public interface ClientReqFormRepository extends JpaRepository<ClientReqForm, Long> {
 
     // Find requests by Client object
     @Query("SELECT r FROM ClientReqForm r WHERE r.client = :client")
     List<ClientReqForm> findByClient(@Param("client") Client client);
+    List<ClientReqForm> findByStatus(String status);
 }
