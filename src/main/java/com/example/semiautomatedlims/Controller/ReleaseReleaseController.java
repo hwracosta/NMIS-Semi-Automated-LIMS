8d409ed0df6579306d65622bff6b88784073b7b2
ljdvr <lddevera@up.edu.ph>
2024-10-22 17:09:16 +0800

    fixed errors

--- src/main/java/com/example/semiautomatedlims/Controller/ReleaseReleaseController.java
+++ src/main/java/com/example/semiautomatedlims/Controller/ReleaseReleaseController.java
@@ -1,14 +1,14 @@
 package com.example.semiautomatedlims.Controller;
 
-import com.example.semiautomatedlims.Entity.ClientReqForm;
-import com.example.semiautomatedlims.Service.ReleaseService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.List;
+import com.example.semiautomatedlims.Entity.ClientReqForm;
+import com.example.semiautomatedlims.Service.ReleaseService;
 
 @Controller
 public class ReleaseReleaseController {
@@ -18,7 +18,7 @@ public class ReleaseReleaseController {
 
     @GetMapping("/RELEASE-release")
     public String showReleaseReleasePage(Model model) {
-        List<String> statuses = Arrays.asList("For Testing", "Sample/s Rejected");
+        List<String> statuses = List.of("For Testing");
         List<ClientReqForm> processedRequests = releaseService.getProcessedRequests(statuses);
         model.addAttribute("requests", processedRequests);
         return "RELEASE-release";
--- src/main/java/com/example/semiautomatedlims/Controller/TRMicroBioController.java
+++ src/main/java/com/example/semiautomatedlims/Controller/TRMicroBioController.java
@@ -1,12 +1,14 @@
 package com.example.semiautomatedlims.Controller;
 
-import com.example.semiautomatedlims.Entity.ClientReqForm;
-import com.example.semiautomatedlims.Service.TestingMicrobioService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
+
+import com.example.semiautomatedlims.Entity.ClientReqForm;
+import com.example.semiautomatedlims.Service.TestingMicrobioService;
 
 @Controller
 public class TRMicroBioController {
@@ -14,11 +16,11 @@ public class TRMicroBioController {
     @Autowired
     private TestingMicrobioService testingMicrobioService;
 
-    @GetMapping("/TR-MicroBio")
+    @GetMapping("/TR-Microbio")
     public String showTRMicrobioPage(Model model) {
         // Fetch only requests that have a status of "For Testing" and microbio_pending as "pending" and transferred
         List<ClientReqForm> resultRequests = testingMicrobioService.findByMicrobioPending("For Testing", "pending");
         model.addAttribute("requests", resultRequests);
-        return "TR-MicroBio";
+        return "TR-Microbio";
     }
 }
