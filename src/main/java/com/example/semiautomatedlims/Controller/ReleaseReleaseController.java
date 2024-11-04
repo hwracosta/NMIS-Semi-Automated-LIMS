05fd30a9dbb2cfb9c60012c58e9d337a8c13ee02
JuyeonCodez <lglumain@up.edu.ph>
2024-11-04 18:50:13 +0800

    Client Details view fixed in Release-release

--- src/main/java/com/example/semiautomatedlims/Controller/ReleaseReleaseController.java
+++ src/main/java/com/example/semiautomatedlims/Controller/ReleaseReleaseController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
