7185b770a2b939abc4a82352ee553d1728487ea9
Daphne <dsbaui@up.edu.ph>
2024-11-04 13:59:31 +0800

    initial back-end for database page

--- src/main/java/com/example/semiautomatedlims/Repository/ReportTestingRepository.java
+++ src/main/java/com/example/semiautomatedlims/Repository/ReportTestingRepository.java
@@ -2,8 +2,23 @@ package com.example.semiautomatedlims.Repository;
 
 import com.example.semiautomatedlims.Entity.ReportTestingSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReportTestingRepository extends JpaRepository<ReportTestingSummary, Long> {
+
+    // Retrieve all microbiological tests
+    @Query("SELECT r FROM ReportTestingSummary r WHERE r.category = 'Microbiological'")
+    List<ReportTestingSummary> findAllMicrobiologicalTests();
+
+    // Retrieve all molecular biology tests
+    @Query("SELECT r FROM ReportTestingSummary r WHERE r.category = 'Molecular Biology'")
+    List<ReportTestingSummary> findAllMolecularBiologyTests();
+
+    // Retrieve all chemical/veterinary drug residue tests
+    @Query("SELECT r FROM ReportTestingSummary r WHERE r.category = 'Chemical/Veterinary Drug Residue'")
+    List<ReportTestingSummary> findAllChemicalTests();
 }
